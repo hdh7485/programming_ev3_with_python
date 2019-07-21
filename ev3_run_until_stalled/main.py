8c7d0e5554d4bc108d9a9cf72b467ec3616d8fda
Donghee Han <hdh7485@gmail.com>
2019-07-22 08:56:45 +0900

    Modify parameters for easy to understand

--- ev3_run_until_stalled/main.py
+++ ev3_run_until_stalled/main.py
@@ -8,5 +8,8 @@ from pybricks.parameters import (Port, Stop, Direction, Button, Color,
 from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
 
-# Write your program here
-brick.sound.beep()
+left_motor = Motor(Port.B)
+left_motor.run_until_stalled(360, Stop.BRAKE) #모터의 최대 출력을 제한하지 않습니다.
+wait(1000)
+left_motor.run_until_stalled(360, Stop.BRAKE, 30) #모터의 최대 출력을 30%로 제한합니다.
+wait(1000)
